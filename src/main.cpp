066c1c1cce2ebe48cb8d1ad7a3b1649524c31f81
benjamin <benjamin.cochery@epitech.eu>
2026-07-02 16:29:40 +0200

    encapsulation de la window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,46 @@
-#include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
+#include <string>
+
+class NewWindow {
+    private:
+        std::string namePage;
+        sf::VideoMode videoMode;
+        sf::RenderWindow window;
+    public:
+        // construteur
+        NewWindow() : videoMode({1920, 1080}), namePage(" ") {}
+        NewWindow(std::string name) : videoMode({1920, 1080}), namePage(name) {}
+        NewWindow(sf::VideoMode mode, std::string name) : videoMode(mode), namePage(name) {}
+
+        // fonction encapsulant la sfml
+        sf::VideoMode getVideoMode() const {
+            return videoMode;
+        }
+        bool windowIsOpen() {
+            if (window.isOpen())
+                return true;
+            return false;
+        }
+        void setWindow() { window.create(videoMode, namePage);}
+        void close() { window.close(); }
+        void clear() { window.clear(); }
+        void display() { window.display(); }
+        std::optional<sf::Event> pollEvent() { return window.pollEvent(); }
+};
 
 int main()
 {
-    cout<<"Hello World !\n";
-    return 0;
+    NewWindow window("firth window");
+    window.setWindow();
+
+    while (window.windowIsOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+        window.clear();
+        window.display();
+    }
 }
